d40df4843b829a818982672471198c80504d7425
Arnat Asanov <asanovarnat2209@gmail.com>
2025-06-24 14:16:05 +0600

    add models.py

--- source/todo_project/tasks/models.py
+++ source/todo_project/tasks/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Сделано'),
+    ]
+
+    description = models.TextField(verbose_name='Описание задачи')
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='new',
+        verbose_name='Статус задачи'
+    )
+    due_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='Дата выполнения'
+    )
+
+    def __str__(self):
+        return f"{self.description[:50]} ({self.get_status_display()})"
