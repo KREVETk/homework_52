ff7e607fddf80365bdb9700f4f12d1890dd92de0
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-04 18:19:01 +0600

    Массовое удаление задач, поле details в форму и подтверждение удаления

--- source/todo_project/tasks/forms.py
+++ source/todo_project/tasks/forms.py
@@ -4,8 +4,16 @@ from .models import Task
 
 class TaskForm(forms.ModelForm):
     status = forms.ChoiceField(choices=Task.STATUS_CHOICES)
-    due_date = forms.DateField(required=False, widget=forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD'}))
+    due_date = forms.DateField(
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD'})
+    )
+    details = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={'placeholder': 'Подробное описание'}),
+        label='Подробное описание'
+    )
 
     class Meta:
         model = Task
-        fields = ['description', 'status', 'due_date']
+        fields = ['description', 'details', 'status', 'due_date']
