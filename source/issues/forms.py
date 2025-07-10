df49f7ba08205ac62bb3f9208d10708d14e43c17
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-11 01:48:55 +0600

    Добавлена форма с полем type_temp ModelMultipleChoiceField с CheckboxSelectMultiple

--- source/issues/forms.py
+++ source/issues/forms.py
@@ -1,32 +1,25 @@
 from django import forms
 from .models import Issue, Status, Type
 
-
 class IssueForm(forms.ModelForm):
     status = forms.ModelChoiceField(
         queryset=Status.objects.all(),
         label="Статус",
         widget=forms.Select(attrs={"class": "form-control"})
     )
-    type = forms.ModelChoiceField(
+    type_temp = forms.ModelMultipleChoiceField(
         queryset=Type.objects.all(),
         label="Тип задачи",
-        widget=forms.Select(attrs={"class": "form-control"})
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "form-check-input"})
     )
 
     class Meta:
         model = Issue
-        fields = ['summary', 'description', 'status', 'type']
+        fields = ['summary', 'description', 'status', 'type_temp']
         widgets = {
             'summary': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
-
-
-
-
-
-
 
 
 
