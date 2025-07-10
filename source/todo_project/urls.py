4a072c7aee851192a0f7f0bdbf303826ea947265
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-11 00:52:00 +0600

    Добавлен базовый шаблон и страница списка задач

--- source/todo_project/urls.py
+++ source/todo_project/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls'))
+    path('', include('tasks.urls')),
+    path('issues/', include('issues.urls'))
 ]
 
 
