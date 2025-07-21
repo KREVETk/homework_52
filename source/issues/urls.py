d196b4bd381dd53ebd463acb23e3e0e91389ca34
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-21 21:22:33 +0600

    Добавлено буллевое поле для мягкого удаления

--- source/issues/urls.py
+++ source/issues/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView,
-    ProjectDeleteView, IssueDetailView, IssueUpdateView, IssueDeleteView)
+    ProjectDeleteView, IssueCreateInProjectView, IssueDetailView, IssueUpdateView, IssueDeleteView)
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
@@ -9,8 +9,9 @@ urlpatterns = [
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('projects/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_edit'),
     path('projects/<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('projects/<int:project_pk>/issues/add/', IssueCreateInProjectView.as_view(), name='issue_add'),
 
     path('issues/<int:pk>/', IssueDetailView.as_view(), name='issue_detail'),
     path('issues/<int:pk>/edit/', IssueUpdateView.as_view(), name='issue_edit'),
-    path('issues/<int:pk>/delete/', IssueDeleteView.as_view(), name='issue_delete'),
+    path('issues/<int:pk>/delete/', IssueDeleteView.as_view(), name='issue_delete')
 ]
